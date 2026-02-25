e107166d0782847c04598bd8bac3e90706963f32
darshan-stdev <darshan@softcolon.com>
2026-02-25 15:49:46 +0530

    fix: body payload size increased for big json data

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,12 @@ const httpServer = createServer(app)
 const PORT = process.env.PORT || 3005
 
 app.use(cors())
-app.use(express.json())
+
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+
 app.use(morgan('dev'))
 
 // Routes
